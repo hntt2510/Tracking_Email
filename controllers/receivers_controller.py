da1fa3a2ad871d21d3ed7b9bc2ed7181737c5144
quocgiahcmut <67970731+quocgiahcmut@users.noreply.github.com>
2025-07-01 14:01:47 +0700

    fix(mistake): un-comment some code

--- controllers/receivers_controller.py
+++ controllers/receivers_controller.py
@@ -17,7 +17,6 @@ def import_receivers():
   file_import = receiver_service.get_import_receiver_file(list_id)
   file_path = f"{config.RECEIVER_FOLDER_STORE}\\{file_import["FILE_NAME"]}.{file_import["FILE_EXT"]}"
   import_data = excel_helper.read_data(file_path, "A:D")
-  print(import_data)
-  #result = receiver_service.insert_or_update_receiver(list_id, import_data)
+  result = receiver_service.insert_or_update_receiver(list_id, import_data)
   
   return redirect_auto_close(True, f"Import receiver list for \"{file_import["LIST_NAME"]}\" successful.")
